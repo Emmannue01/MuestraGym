edc31b30993d6a72d2d0ae65ef659ee3f7103adf
Emmannue01 <joabemmanue01@gmail.com>
2025-08-17 21:46:35 -0600

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,9 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBE7XnFDoaC7qPY8nykIGI-SCCMM6P_iG0", 
-    authDomain: "gymrats-d16eb.firebaseapp.com",
-    projectId: "gymrats-d16eb",
-    storageBucket: "gymrats-d16eb.appspot.com",
-    messagingSenderId: "106526343668278361492",
-    appId: "1:106526343668278361492:web:abcd1234efgh5678"
+  apiKey: "AIzaSyBE7XnFDoaC7qPY8nykIGI-SCCMM6P_iG0",
+  authDomain: "gymrats-d16eb.firebaseapp.com",
+  databaseURL: "https://gymrats-d16eb-default-rtdb.firebaseio.com",
+  projectId: "gymrats-d16eb",
+  storageBucket: "gymrats-d16eb.firebasestorage.app",
+  messagingSenderId: "527362115499",
+  appId: "1:527362115499:web:bf44ba5c9bfeaf011874ee",
+  measurementId: "G-3TB5ZMZGWJ"
 };
 export { firebaseConfig };
